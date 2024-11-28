b803b2128df154b8aa45385ff52fd4c0032bd2c6
Nathali-Huamani28 <katyhuamani634@gmail.com>
2024-11-28 09:29:31 -0500

    hola

--- AlquilerCarros/Alquiler.cs
+++ AlquilerCarros/Alquiler.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AlquilerCarros
+﻿namespace AlquilerDeCarros
 {
-    internal class Alquiler
+    public class Alquiler
     {
+        public int Numero { get; set; }
+        public Carro CarroAlq { get; set; }
+        public Cliente Contratante { get; set; }
+        public double Precio { get; set; }
+
+        public override string ToString()
+        {
+            return $"Alquiler #{Numero}, {CarroAlq}, Cliente: {Contratante.Nombre}, Precio: {Precio:C}";
+        }
     }
 }
